33ff4e2e2e7e942cb7874954b6ce40c282d7ce7f
josetellis-2024 <awsjoetellis@gmail.com>
2026-09-13 10:39:54 +0530

    multi added

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 import pytest
-from app import add, subtract
+from app import add, subtract, multiply
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
@@ -10,4 +10,8 @@ def test_subtract():
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
     assert subtract(-5, -3) == -2
-    
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 5) == 0
+    assert multiply(-5, -5) == 25
